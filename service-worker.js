1d0692dd7a34b2bd36b504691661e1ff66f11c24
tmdgh9693 <tmdgh9693@gmail.com>
2026-07-10 13:54:25 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,72 @@
+const CACHE_NAME = 'dept-calendar-v3';
+const APP_FILES = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './css/style.css',
+  './css/calendar.css',
+  './css/meeting.css',
+  './css/trip.css',
+  './css/archive.css',
+  './css/responsive.css',
+  './js/utils.js',
+  './js/app-install.js',
+  './js/app.js',
+  './js/auth.js',
+  './js/firebase.js',
+  './js/firebase-config.js',
+  './js/calendar.js',
+  './js/meeting.js',
+  './js/trip.js',
+  './js/archive.js',
+  './js/settings.js',
+  './js/hwpx.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request).then(response => {
+        if (response.ok && event.request.url.startsWith(self.location.origin)) {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        }
+        return response;
+      });
+    })
+  );
+});
