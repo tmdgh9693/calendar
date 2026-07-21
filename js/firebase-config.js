90cb343722034edbfb9afc61a69ff11d252b3c68
tmdgh9693 <tmdgh9693@gmail.com>
2026-07-21 11:08:37 +0900

    Delete js directory

--- js/firebase-config.js
+++
@@ -1,9 +0,0 @@
-window.firebaseConfig = {
-  apiKey: "AIzaSyDxc-G2wL46Wxl5Hlbu5kUuhdr7pzbDt-g",
-  authDomain: "aton-calendar.firebaseapp.com",
-  projectId: "aton-calendar",
-  storageBucket: "aton-calendar.firebasestorage.app",
-  messagingSenderId: "401895574513",
-  appId: "1:401895574513:web:ac12a2010cc6e1458c1250",
-  measurementId: "G-PT71F7YNG8"
-};
